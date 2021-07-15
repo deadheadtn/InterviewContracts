3a1b3521ebdb00066299e5079b3b750eb55cd6b1
Nejm <nejmeddine.khechine@esprit.tn>
2021-07-15 19:57:12 +0100

    safemath

--- ERC.sol
+++ ERC.sol
@@ -2,7 +2,9 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-
+interface tokenRecipient {
+    function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external;
+}
 contract ERC20 {
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
