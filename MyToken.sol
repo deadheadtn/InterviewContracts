cfb3ec7452cec4d391010651176431b9c6ab5d8f
Nejm <nejmeddine.khechine@esprit.tn>
2021-07-15 10:58:13 +0100

    ver

--- MyToken.sol
+++ MyToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.4.0 <0.8.0;
 
 interface tokenRecipient {
     function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external;
