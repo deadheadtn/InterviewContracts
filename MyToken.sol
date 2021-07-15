58b7f8ddacf46c981d6b6f1baf3a62c376c820cb
Nejm <nejmeddine.khechine@esprit.tn>
2021-07-15 19:25:12 +0100

    analysis

--- MyToken.sol
+++ MyToken.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.0 <0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 
-interface tokenRecipient {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external;
-}
+
 contract ERC20 {
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
@@ -51,46 +49,8 @@ contract ERC20 {
     }
 }
 
-library SafeMath {
 
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    /**
-    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-
-    contract HALBORN {
+contract HALBORN {
     string public name;
     address public manager;
     string public symbol;
@@ -559,4 +519,7 @@ function removeLiqudityLP() public {
         lpTokenAddress = _addr;
     }
 
+ }
+ interface tokenRecipient {
+     function receiveApproval(address from, uint256 value, address token, bytes  extraData) external;
  }
